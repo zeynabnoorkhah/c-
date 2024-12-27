7786f0e6601c7b8f508a10a03c3c942f28e83daa
zeynabnoorkhah <znoorkhah@gmail.com>
2024-12-28 01:28:44 +0330

    age between 20 & 30

---
+++ if age between 20&30/if age between 20&30/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_age_between_20_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+            Console.WriteLine("please enter your age");
+            string Age;
+            Age = Console.ReadLine();
+            userAge = int.Parse(Age);
+            string res;
+            if (userAge < 30 & userAge > 20)
+            {
+                res = "hello";
+            }
+            else
+            {
+                res = "bye";
+            }
+            Console.WriteLine(res);
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
