4fa6870d64a21bab712b121c0e52434712220e52
zeynabnoorkhah <znoorkhah@gmail.com>
2024-12-28 01:27:26 +0330

    average

---
+++ average/average/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum, avg;
+            Console.WriteLine("please enter 2 numbers");
+            a = Convert.ToInt32(System.Console.ReadLine());
+            b = Convert.ToInt32(System.Console.ReadLine());
+            sum = a + b;
+            avg = sum / 2;
+            System.Console.Write(avg);
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ average/average/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("average")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("average")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("49702fb4-2f2f-42b6-84c9-fd641a4d9754")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
